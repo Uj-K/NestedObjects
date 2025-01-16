ad95e4af42b6acde6162e1181e48dfa41c30cd6b
Uj-K <umkrall@gmail.com>
2025-01-16 15:49:47 -0800

    Created student and advisor classes

---
+++ NestedObjects/Advisor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represent a college advisor
+    /// </summary>
+    public class Advisor
+    {
+        /// <summary>
+        /// The legal first and last name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// The work email of the Advisor
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// The location of the advisor on campus
+        /// </summary>
+        public string OfficeLocation { get; set; }
+    }
+
+}
---
+++ NestedObjects/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents a individual college student
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// The legal first name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The legal last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The assigned school email address
+        /// </summary>
+        public string SchoolEmail { get; set; }
+
+        /// <summary>
+        /// The Phone number of the student
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// The advisor that assigned to the student
+        /// </summary>
+        public Advisor AssignedAdvisor { get; set; }
+    }
+}
